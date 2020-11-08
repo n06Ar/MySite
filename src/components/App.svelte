e9bfc246622525596451159c9424c237dadb4f11
NobuhitoArai <nobht.arai@gmail.com>
2020-11-08 23:54:23 +0900

    change directory.

---
+++ src/components/App.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Header from './common/Header.svelte'
+    import Footer from './common/Footer.svelte'
+    import routes from '..//utility/Router.ts'
+    import Router, {
+        link,
+        push,
+        pop,
+        replace,
+        location,
+        querystring,
+    } from 'svelte-spa-router'
+</script>
+
+<style lang="scss">
+    .main {
+        flex: 1;
+    }
+</style>
+
+<Header />
+<main class="main">
+    <Router routes="{routes}" />
+</main>
+<Footer />
---
+++ src/components/pages/Home.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+
+</script>
+
+<style lang="scss">
+    .home {
+        width: 80%;
+        max-width: 1440px;
+        margin: auto;
+    }
+
+    .home__content {
+        display: block;
+
+        &:not(:first-child) {
+            margin-top: 3rem;
+        }
+    }
+</style>
+
+<section class="home">
+    <h2>
+        <span data-uk-icon="icon: home; ratio: 1.5" aria-hidden="true"></span> Home
+    </h2>
+    <artcle class="home__content">
+        <h3>ここはなんですか？</h3>
+        <p>
+            私、N06ARが、「そういえば個人サイトを作っていないな」とふと思い作ったサイトがここです。<br
+            /> 残念ながら、今はコンテンツが何もないですが、私が過去に作っていたプログラムや書いた記事をここにまとめる予定です。
+        </p>
+        <div>
+            <a href="https://github.com/n06ar/mysite">
+                <img
+                    src="https://github-link-card.s3.ap-northeast-1.amazonaws.com/n06ar/mysite.png"
+                    width="460"
+                    alt="このサイトのリポジトリ"
+                />
+            </a>
+        </div>
+    </artcle>
+    <artcle class="home__content">
+        <h3>このサイトを作るのに使っているもの</h3>
+        <p>
+            このサイトを作るにあたり次のFrameworkやLibrary、Packageを使いました
+        </p>
+        <ul>
+            <li><a href="https://svelte.dev/">Svelte</a></li>
+            <li><a href="https://parceljs.org/">PARCEL</a></li>
+            <li><a href="https://www.typescriptlang.org/">TypeScript</a></li>
+            <li><a href="https://sass-lang.com/">Sass</a></li>
+            <li><a href="https://getuikit.com">UIkit</a></li>
+        </ul>
+    </artcle>
+</section>
