fabf62a3211806815573dcf16bfccf4ea327ccd8
NobuhitoArai <nobht.arai@gmail.com>
2020-11-09 00:06:08 +0900

    add snowpack files.

---
+++ snowpack.config.js
@@ -0,0 +1,22 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-svelte',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
+    "@snowpack/plugin-sass"
+  ],
+  install: [
+    /* ... */
+  ],
+  buildOptions: {
+    out: "docs"
+  },
+  alias: {
+    /* ... */
+  },
+};
