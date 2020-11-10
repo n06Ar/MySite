35ebf40183700e4dd10b35cb462f42e06cb2c5ba
NobuhitoArai <nobht.arai@gmail.com>
2020-11-10 23:50:06 +0900

    fix snowpack specifications.

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,15 +1,8 @@
 <script lang="ts">
     import Header from './common/Header.svelte'
     import Footer from './common/Footer.svelte'
-    import routes from '..//utility/Router.ts'
-    import Router, {
-        link,
-        push,
-        pop,
-        replace,
-        location,
-        querystring,
-    } from 'svelte-spa-router'
+    import routes from '../utility/Router.ts'
+    import Router from 'svelte-spa-router'
 </script>
 
 <style lang="scss">
--- src/components/common/Header.svelte
+++ src/components/common/Header.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { link } from 'svelte-spa-router'
+    import Router, { link } from 'svelte-spa-router'
 </script>
 
 <style lang="scss">
