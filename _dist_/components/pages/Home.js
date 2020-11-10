cefaa38c0568d794c55d846e81db201081046dab
n06Ar <n06Ar@users.noreply.github.com>
2020-11-10 14:00:15 +0000

    deploy: 07cbfd0b6de5d0a306fcb9e8006205434c4a4c9a

--- _dist_/components/pages/Home.js
+++ _dist_/components/pages/Home.js
@@ -19,16 +19,16 @@ function create_fragment(ctx) {
 			section = element("section");
 
 			section.innerHTML = `<h2><span data-uk-icon="icon: home; ratio: 1.5" aria-hidden="true"></span> Home</h2> 
-    <artcle class="home__content svelte-bnehzi"><h3>ここはなんですか？</h3> 
+    <article class="home__content svelte-bnehzi"><h3>ここはなんですか？</h3> 
         <p>私、N06ARが、「そういえば個人サイトを作っていないな」とふと思い作ったサイトがここです。<br/> 残念ながら、今はコンテンツが何もないですが、私が過去に作っていたプログラムや書いた記事をここにまとめる予定です。</p> 
-        <div><a href="https://github.com/n06ar/mysite"><img src="https://github-link-card.s3.ap-northeast-1.amazonaws.com/n06ar/mysite.png" width="460" alt="このサイトのリポジトリ"/></a></div></artcle> 
-    <artcle class="home__content svelte-bnehzi"><h3>このサイトを作るのに使っているもの</h3> 
+        <div><a href="https://github.com/n06ar/mysite"><img src="https://github-link-card.s3.ap-northeast-1.amazonaws.com/n06ar/mysite.png" width="460" alt="このサイトのリポジトリ"/></a></div></article> 
+    <article class="home__content svelte-bnehzi"><h3>このサイトを作るのに使っているもの</h3> 
         <p>このサイトを作るにあたり次のFrameworkやLibrary、Packageを使いました</p> 
         <ul><li><a href="https://svelte.dev/">Svelte</a></li> 
-            <li><a href="https://parceljs.org/">PARCEL</a></li> 
+            <li><a href="https://www.snowpack.dev">Snowpack</a></li> 
             <li><a href="https://www.typescriptlang.org/">TypeScript</a></li> 
             <li><a href="https://sass-lang.com/">Sass</a></li> 
-            <li><a href="https://getuikit.com">UIkit</a></li></ul></artcle>`;
+            <li><a href="https://getuikit.com">UIkit</a></li></ul></article>`;
 
 			attr(section, "class", "home svelte-bnehzi");
 		},
