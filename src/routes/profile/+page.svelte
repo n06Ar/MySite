a750cf514fc52cea51c135dbea40a1d0cd04028f
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2023-07-22 07:18:16 +0900

    🔥remove duplicate Academic Background.

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -54,16 +54,6 @@
                 </dd>
             </dl>
             <dl>
-                <dt>Academic Background</dt>
-                <dd>
-                    <ul>
-                        <li>浦和実業学園高等学校 普通科</li>
-                        <li>拓殖大学 工学部 情報工学科</li>
-                        <li>拓殖大学大学院 工学研究科 情報デザイン工学専攻</li>
-                    </ul>
-                </dd>
-            </dl>
-            <dl>
                 <dt>Programming Languages</dt>
                 <dd>
                     <ul class="profile-content__list -two-rows">
