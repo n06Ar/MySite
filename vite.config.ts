d6f64139a4b556eb8e8189706fe17e44019aa63d
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-07-01 22:57:38 +0900

    🚨 fix indent.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    resolve: {
-        alias: {
-            '@components/': `${__dirname}/src/components/`,
-        },
-    },
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'@components/': `${__dirname}/src/components/`,
+		},
+	},
 })
