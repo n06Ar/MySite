b929e16d4745108a96ef0de0425c6c8028cbf4de
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-08-07 22:06:02 +0900

    🖼️ add favicon and mobile icons.

--- src/components/common/Header.svelte
+++ src/components/common/Header.svelte
@@ -1,81 +1,26 @@
+<script>
+	import {Navbar, NavBrand, NavUl, NavLi} from 'flowbite-svelte'
+</script>
+
 <header
-	data-uk-sticky='sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky'
+
 >
-	<nav class='uk-navbar-container uk-margin' data-uk-navbar>
-		<div class='header'>
-			<div class='uk-navbar-left'>
-				<h1 class='header__title uk-logo'>
-					<a href='/' class='header-title__text'> NaoNao </a>
-				</h1>
-			</div>
+	<Navbar>
+		<NavBrand href='/'>
+			NaoNao
+		</NavBrand>
 
-			<div class='uk-navbar-right'>
-				<ul class='header__nav uk-navbar-nav'>
-					<li>
-						<a href='/' class='header-nav__link'>
-							<span uk-icon='home' aria-hidden='true'></span>
-							<span class='header-nav__letter-gap'></span> Home
-						</a>
-					</li>
-					<li>
-						<a href='/profile' class='header-nav__link'>
-							<span uk-icon='user' aria-hidden='true'></span>
-							<span class='header-nav__letter-gap'></span> Profile
-						</a>
-					</li>
-					<li>
-						<a href='/science-room' class='header-nav__link'>
-							<span uk-icon='user' aria-hidden='true'></span>
-							<span class='header-nav__letter-gap'></span> Science Room
-						</a>
-					</li>
-					<li>
-						<a
-							href='https://n06ar.hatenablog.com'
-							class='header-nav__link'
-						>
-							<span uk-icon='bookmark' aria-hidden='true'></span>
-							<span class='header-nav__letter-gap'></span> Blog
-							<span class='header-nav__letter-gap'></span>
-							<span uk-icon='icon: push;' aria-hidden='true'
-							></span>
-						</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+		<NavUl>
+			<NavLi href='/'>
+				Home
+			</NavLi>
+			<NavLi href='/profile'>
+				Profile
+			</NavLi>
+			<NavLi href='https://n06ar.hatenablog.com'>
+				Blog
+			</NavLi>
+		</NavUl>
+	</Navbar>
+
 </header>
-
-<style lang='scss'>
-	.header {
-		display: flex;
-		width: 80%;
-		max-width: 1440px;
-		margin: auto;
-	}
-
-	.header__title {
-		margin: auto 0;
-	}
-
-	.header-title__text {
-		font-size: 2rem;
-		font-weight: bold;
-		color: #333333;
-		text-decoration: none;
-
-		&:link {
-			color: #333333;
-		}
-	}
-
-	.header-nav__link {
-		font-size: 1rem;
-		text-decoration: none;
-	}
-
-	.header-nav__letter-gap {
-		margin-right: 0.5rem;
-	}
-</style>
