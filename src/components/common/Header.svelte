060ff00f7a47cc1444512f8bad8fdb1f6be8abe8
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-08-08 05:34:44 +0900

    🔥remove flowbite-svelte

--- src/components/common/Header.svelte
+++ src/components/common/Header.svelte
@@ -1,26 +1,5 @@
-<script>
-	import {Navbar, NavBrand, NavUl, NavLi} from 'flowbite-svelte'
-</script>
 
 <header
 
 >
-	<Navbar>
-		<NavBrand href='/'>
-			NaoNao
-		</NavBrand>
-
-		<NavUl>
-			<NavLi href='/'>
-				Home
-			</NavLi>
-			<NavLi href='/profile'>
-				Profile
-			</NavLi>
-			<NavLi href='https://n06ar.hatenablog.com'>
-				Blog
-			</NavLi>
-		</NavUl>
-	</Navbar>
-
 </header>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'tailwindcss'
 import flowbitePlugin from 'flowbite/plugin'
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}','./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	darkMode: 'selector',
 	theme: {
 		extend: {
