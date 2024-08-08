a4b54033f94a75e9ac49444c54570d9a0dba5878
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-08-09 02:42:08 +0900

    ✨ add app.css import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
-<script lang="ts">
-    import Header from '@components/common/Header.svelte'
-    import Footer from '@components/common/Footer.svelte'
+<script lang='ts'>
+	import '../app.css'
+	import Header from '@components/common/Header.svelte'
+	import Footer from '@components/common/Footer.svelte'
 </script>
 
 <svelte:head>
