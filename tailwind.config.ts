8fad0425a023972fd131fb17cdb87b4a2c212167
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-08-09 02:39:15 +0900

    🔧 re-add flowbite-svelte in content.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,10 @@ import flowbitePlugin from 'flowbite/plugin'
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	darkMode: 'selector',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
+	],
 	theme: {
 		extend: {
 			colors: {
