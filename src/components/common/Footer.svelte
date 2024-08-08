f645fc495308e2ddc8e622bf73e96a67f8b73529
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-08-09 03:49:34 +0900

    ✨ replace flowbite

--- src/components/common/Footer.svelte
+++ src/components/common/Footer.svelte
@@ -1,24 +1,7 @@
 <script lang="ts">
+	import { Footer, FooterCopyright } from 'flowbite-svelte'
 </script>
 
-<footer class="footer">
-    <p class="footer__copyright">
-        <small>Copyright (c) NaoNao All Rights Reserved.</small>
-    </p>
-</footer>
-
-<style lang="scss">
-    .footer {
-        position: sticky;
-        top: 100vh;
-        padding: 1rem 0;
-        background: #e2e2e2;
-    }
-
-    .footer__copyright {
-        width: 80%;
-        max-width: 1440px;
-        margin: auto;
-        text-align: center;
-    }
-</style>
+<Footer class='pt-5 pb-5 flex-grow'>
+	<FooterCopyright by='NaoNao' />
+</Footer>
