a7adac0c1f68a84e332cd9a97579eab160d00e96
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-08-09 03:57:04 +0900

    ✨ replace flowbite

--- src/components/common/Header.svelte
+++ src/components/common/Header.svelte
@@ -1,5 +1,37 @@
+<script>
+	import { Navbar, NavBrand, NavUl, NavLi, Heading } from 'flowbite-svelte'
+	import { BookSolid, HomeOutline, ProfileCardOutline } from 'flowbite-svelte-icons'
+</script>
 
-<header
+<header class='sticky top-0 left-0 right-0'>
+	<Navbar color='blue' shadow='true'>
+		<NavBrand href='/' class='menu-brand'>
+			<Heading tag='h1' class='text-2xl text-primary-900'>NaoNao</Heading>
+		</NavBrand>
 
->
+		<NavUl>
+			<NavLi href='/' class='menu-nav__list'>
+				<HomeOutline class='inline-block' />
+				Home
+			</NavLi>
+			<NavLi href='/profile' class='menu-nav__list'>
+				<ProfileCardOutline class='inline-block' />
+				Profile
+			</NavLi>
+			<NavLi href='https://n06ar.hatenablog.com' class='menu-nav__list'>
+				<BookSolid class='inline-block' />
+				Blog
+			</NavLi>
+		</NavUl>
+	</Navbar>
 </header>
+
+<style>
+	.menu-brand {
+		@apply text-2xl text-primary-900 dark:text-primary-50
+	}
+
+	.menu-nav__list {
+		@apply text-primary-950 dark:text-primary-50
+	}
+</style>
