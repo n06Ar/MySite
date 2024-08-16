9f773a86c8aa1ff76070f6e1c6bd1fce255672e6
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-08-16 15:13:20 +0900

    💄add class of background and text color

--- src/components/common/Header.svelte
+++ src/components/common/Header.svelte
@@ -4,34 +4,24 @@
 </script>
 
 <header class='sticky top-0 left-0 right-0'>
-	<Navbar color='blue' shadow='true'>
-		<NavBrand href='/' class='menu-brand'>
-			<Heading tag='h1' class='text-2xl text-primary-900'>NaoNao</Heading>
+	<Navbar color='primary' shadow='true' class='bg-primary-600'>
+		<NavBrand href='/'>
+			<Heading tag='h1' class='text-2xl text-primary-950 dark:text-primary-50'>NaoNao</Heading>
 		</NavBrand>
 
 		<NavUl>
-			<NavLi href='/' class='menu-nav__list'>
+			<NavLi href='/' activeClass='text-primary-800' nonActiveClass='text-primary-950 dark:text-primary-50'>
 				<HomeOutline class='inline-block' />
 				Home
 			</NavLi>
-			<NavLi href='/profile' class='menu-nav__list'>
+			<NavLi href='/profile' nonActiveClass='text-primary-950 dark:text-primary-50'>
 				<ProfileCardOutline class='inline-block' />
 				Profile
 			</NavLi>
-			<NavLi href='https://n06ar.hatenablog.com' class='menu-nav__list'>
+			<NavLi href='https://n06ar.hatenablog.com' nonActiveClass='text-primary-950 dark:text-primary-50'>
 				<BookSolid class='inline-block' />
 				Blog
 			</NavLi>
 		</NavUl>
 	</Navbar>
 </header>
-
-<style>
-	.menu-brand {
-		@apply text-2xl text-primary-900 dark:text-primary-50
-	}
-
-	.menu-nav__list {
-		@apply text-primary-950 dark:text-primary-50
-	}
-</style>
