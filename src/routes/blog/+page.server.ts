00301b23c96b1db365d2d473d3ce9230a1e5e8a4
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-09-02 02:06:13 +0900

    🥅 add error

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,3 +1,4 @@
+import { error } from '@sveltejs/kit'
 import { getBlogPageList } from '$lib/microCMS'
 // @ts-ignore
 import type { PageServerLoad } from './$types'
@@ -8,8 +9,11 @@ export const load: PageServerLoad = async ({ url }: any) => {
 	if (url.searchParams.has('page')) {
 		queries.offset = url.searchParams.get('page')
 	}
-
-	return await getBlogPageList(queries)
+	try {
+		return await getBlogPageList(queries)
+	} catch (e) {
+		error(500)
+	}
 }
 
 export const prerender = true
