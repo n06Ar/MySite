ac83472232d8fa9390c01d476da674676d6cf4f9
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-09-01 09:49:52 +0900

    ✨remove color and replace blog url.

--- src/components/common/Header.svelte
+++ src/components/common/Header.svelte
@@ -4,21 +4,21 @@
 </script>
 
 <header class='sticky top-0 left-0 right-0'>
-	<Navbar color='primary' shadow='true' class='bg-primary-600'>
+	<Navbar color='primary' shadow='true'>
 		<NavBrand href='/'>
-			<Heading tag='h1' class='text-2xl text-primary-50 '>NaoNao</Heading>
+			<Heading tag='h1' class='text-2xl'>NaoNao</Heading>
 		</NavBrand>
 
 		<NavUl>
-			<NavLi href='/' activeClass='text-primary-800' nonActiveClass='text-primary-50 '>
+			<NavLi href='/'>
 				<HomeOutline class='inline-block' />
 				Home
 			</NavLi>
-			<NavLi href='/playground' nonActiveClass='text-primary-50 '>
+			<NavLi href='/playground'>
 				<WandMagicSparklesOutline class='inline-block' />
 				Playground
 			</NavLi>
-			<NavLi href='https://n06ar.hatenablog.com' nonActiveClass='text-primary-50 '>
+			<NavLi href='/blog'>
 				<BookSolid class='inline-block' />
 				Blog
 			</NavLi>
