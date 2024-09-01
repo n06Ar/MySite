19bc7b11ce76635b023abf3e6dc01716decd966a
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-09-02 04:21:25 +0900

    🚚move blog page list

--- src/routes/blog/[page]/+page.server.ts
+++ src/routes/blog/[page]/+page.server.ts
@@ -2,9 +2,16 @@ import { error } from '@sveltejs/kit'
 import { getBlogPageList } from '$lib/microCMS'
 import type { PageServerLoad } from './$types'
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({
+	params,
+}: {
+	params: { page: number }
+}) => {
 	const queries: { offset: number; limit: number } = { offset: 0, limit: 50 }
 
+	if (params.page) {
+		queries.offset = params.page * queries.limit
+	}
 	try {
 		return await getBlogPageList(queries)
 	} catch (e) {
