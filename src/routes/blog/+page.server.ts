8b793ae50780aa5226dc6d7631ef4b2870a2de93
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-09-02 02:08:05 +0900

    🙈remove ts ignore

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,6 +1,5 @@
 import { error } from '@sveltejs/kit'
 import { getBlogPageList } from '$lib/microCMS'
-// @ts-ignore
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async ({ url }: any) => {
