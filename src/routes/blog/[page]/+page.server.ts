9bbbd43a2aa57c5b63f259f92d5905cd8e6e1014
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-09-02 00:21:34 +0900

    ✨add blog page.

---
+++ src/routes/blog/[page]/+page.server.ts
@@ -0,0 +1,25 @@
+import { error } from '@sveltejs/kit'
+
+import { getBlogPageDetail } from '$lib/microCMS'
+// @ts-ignore
+import type { PageServerLoad } from './$types'
+
+export const load: PageServerLoad = async ({ params, url }: any) => {
+	try {
+		const queries: { draftKey?: string } = {}
+
+		if (url.searchParams.has('draftKey')) {
+			queries.draftKey = url.searchParams.get('draftKey')
+		}
+
+		return await getBlogPageDetail(params.page, queries)
+	} catch (e: unknown) {
+		let status = 500
+		if ((e as Error).message === 'Not found') {
+			status = 404
+		}
+		error(status)
+	}
+}
+
+export const prerender = true
---
+++ src/routes/blog/[page]/+page.svelte
@@ -0,0 +1,132 @@
+<script lang='ts'>
+	import { Badge, Heading } from 'flowbite-svelte'
+	import { TagSolid } from 'flowbite-svelte-icons'
+	import { load } from 'cheerio'
+	import highlight from 'highlight.js'
+
+	import type { PageData } from './$types'
+
+	export let data: PageData
+
+	let article = ''
+	let description = ''
+	if (data.content) {
+		const cheeArticle = load(data.content)
+		cheeArticle('pre code').each((_, elm) => {
+			const result = highlight.highlightAuto(cheeArticle(elm).text())
+			cheeArticle(elm).html(result.value)
+			cheeArticle(elm).addClass('hljs')
+		})
+		article = cheeArticle.html()
+
+		const cheeDescription = load(data.content)
+		description = cheeDescription('*').text().substring(0, 50)
+	}
+
+	export { article, description }
+</script>
+
+<svelte:head>
+	<title>{data.title} | NaoNao Blog</title>
+	<meta name='description' content='{description}' />
+</svelte:head>
+
+<article>
+	{#if data.eyecatch}
+		<img class='w-full' src={data.eyecatch?.url ?? "/images/No_Image.png"}
+			 alt='{data.title}' />
+	{/if}
+
+	<div class='mt-8 mx-4 sm:mx-auto'>
+		<Heading tag='h1' class='text-4xl mb-8'>{data.title}</Heading>
+
+		<main class='content'>
+			{@html article}
+		</main>
+
+		<div class='flex justify-between flex-wrap'>
+			{#if data.category}
+				<Badge border class='ml-auto'>
+					<TagSolid class='mr-1' />
+					{data.category.name}
+				</Badge>
+			{/if}
+		</div>
+	</div>
+
+</article>
+
+<style lang='scss'>
+	@import 'highlight.js/styles/tokyo-night-dark.min.css';
+
+	.content {
+		:global(h1) {
+			@apply text-4xl mb-2;
+
+			&::before {
+				content: "#";
+				@apply text-primary-800 dark:text-primary-500 mr-1
+			}
+		}
+
+		:global(h2) {
+			@apply text-3xl mb-2;
+
+			&::before {
+				content: "##";
+				@apply text-primary-800 dark:text-primary-500 mr-1
+			}
+		}
+
+		:global(h3) {
+			@apply text-2xl mb-2;
+
+			&::before {
+				content: "###";
+				@apply text-primary-800 dark:text-primary-500 mr-1
+			}
+		}
+
+		:global(h4) {
+			@apply text-xl mb-2;
+
+			&::before {
+				content: "###";
+				@apply text-primary-800 dark:text-primary-500 mr-1
+			}
+		}
+
+		:global(h5) {
+			@apply text-lg mb-2;
+
+			&::before {
+				content: "###";
+				@apply text-primary-800 dark:text-primary-500 mr-1
+			}
+		}
+
+		:global(a) {
+			@apply hover:underline text-primary-600 dark:text-primary-500;
+		}
+
+		:global(blockquote) {
+			@apply p-4 pr-0 mt-1 mb-1 border-l-2 border-primary-400;
+		}
+
+		:global(pre) {
+			@apply my-4 w-full border-8 border-primary-950 rounded-md bg-primary-950;
+
+			:global(code) {
+				@apply my-1 mx-3 text-primary-50;
+			}
+		}
+
+		:global(ul) {
+			@apply pl-5 list-disc
+		}
+
+		:global(ol) {
+			@apply pl-6 list-decimal
+		}
+	}
+</style>
