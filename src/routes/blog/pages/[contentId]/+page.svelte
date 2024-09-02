b246d7a22876c9b17ae665f1cbcb635ce8c9b12b
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-09-03 01:58:10 +0900

    🐛fix css path

--- src/routes/blog/pages/[contentId]/+page.svelte
+++ src/routes/blog/pages/[contentId]/+page.svelte
@@ -57,7 +57,7 @@
 </article>
 
 <style lang='scss'>
-	@import '../../../../../node_modules/highlight.js/styles/tokyo-night-dark.min.css';
+	@import 'highlight.js/styles/tokyo-night-dark.min.css';
 
 	.content {
 		:global(h1) {
