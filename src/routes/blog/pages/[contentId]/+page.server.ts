400a02244f20104f1fa2bbacf81ba616242c57da
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-09-03 01:06:04 +0900

    ✨add type and change path parameter.

--- src/routes/blog/pages/[contentId]/+page.server.ts
+++ src/routes/blog/pages/[contentId]/+page.server.ts
@@ -2,11 +2,15 @@ import { error } from '@sveltejs/kit'
 import { getBlogPageDetail } from '$lib/microCMS'
 import type { PageServerLoad } from './$types'
 
-export const load: PageServerLoad = async ({ params }: { params: object }) => {
-	try {
-		const queries: { draftKey?: string } = {}
+export const load: PageServerLoad = async ({
+	params,
+}: {
+	params: { contentId: string }
+}) => {
+	const queries: { draftKey?: string } = {}
 
-		return await getBlogPageDetail(params.page, queries)
+	try {
+		return await getBlogPageDetail(params.contentId, queries)
 	} catch (e: unknown) {
 		let status = 500
 		if ((e as Error).message === 'Not found') {
