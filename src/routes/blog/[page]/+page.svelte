bcd3085a7c3906506845fdd567bf96daa4345856
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-09-03 21:40:01 +0900

    ✨add key

--- src/routes/blog/[page]/+page.svelte
+++ src/routes/blog/[page]/+page.svelte
@@ -18,20 +18,22 @@
 
 <section class='flex flex-wrap gap-4 md:gap-12 px-4 sm:px-auto'>
 	{#each data.contents as content}
-		<a href='/blog/pages/{content.id}'
-		   class='bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 rounded-lg border border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 shadow-md w-full sm:w-1/4 md:w-1/4 max-w-sm flex flex-col hover:bg-gray-100 dark:hover:bg-gray-700'
-		>
-			<img src='{content.eyecatch?.url ?? "/images/No_Image.png"}'
-				 alt='{content.eyecatch ? content.title : "No Image"}'
-				 class='rounded-t-lg object-cover aspect-video'>
-			<div class='h-full p-4 sm:p-6 flex flex-col justify-between'>
-				<Heading tag='h3' class='mb-4 text-xl'>{content.title}</Heading>
-				<Button class='w-fit mt-auto'>
-					Read more
-					<ArrowRightOutline class='w-6 h-6 ms-2 text-white' />
-				</Button>
-			</div>
-		</a>
+		{#key content.id}
+			<a href='/blog/pages/{content.id}'
+			   class='bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 rounded-lg border border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 shadow-md w-full sm:w-1/4 md:w-1/4 max-w-sm flex flex-col hover:bg-gray-100 dark:hover:bg-gray-700'
+			>
+				<img src='{content.eyecatch?.url ?? "/images/No_Image.png"}'
+					 alt='{content.eyecatch ? content.title : "No Image"}'
+					 class='rounded-t-lg object-cover aspect-video'>
+				<div class='h-full p-4 sm:p-6 flex flex-col justify-between'>
+					<Heading tag='h3' class='mb-4 text-xl'>{content.title}</Heading>
+					<Button class='w-fit mt-auto'>
+						Read more
+						<ArrowRightOutline class='w-6 h-6 ms-2 text-white' />
+					</Button>
+				</div>
+			</a>
+		{/key}
 	{/each}
 </section>
 
