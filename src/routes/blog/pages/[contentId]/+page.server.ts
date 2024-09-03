66140be045adee3d172f45bae47a5195c1dc9fbe
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2024-09-03 21:38:19 +0900

    🔥remove draft mode

--- src/routes/blog/pages/[contentId]/+page.server.ts
+++ src/routes/blog/pages/[contentId]/+page.server.ts
@@ -7,10 +7,8 @@ export const load: PageServerLoad = async ({
 }: {
 	params: { contentId: string }
 }) => {
-	const queries: { draftKey?: string } = {}
-
 	try {
-		return await getBlogPageDetail(params.contentId, queries)
+		return await getBlogPageDetail(params.contentId)
 	} catch (e: unknown) {
 		let status = 500
 		if ((e as Error).message === 'Not found') {
