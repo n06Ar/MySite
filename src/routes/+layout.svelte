44decd9b3cb463d83bf52568e361a40a8bd80ca0
Nobuhito Arai <nobuhito.arai@n06ar.me>
2025-03-10 20:11:41 +0900

    Add Biome CI (#49)
    * 👷 add biome ci.
    
    * 🎨fix biome
    
    * 🎨fix biome
    
    * 🎨fix biome

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang='ts'>
-	import '../app.css'
-	import Footer from '@components/common/Footer.svelte'
-	import Header from '@components/common/Header.svelte'
+import '../app.css'
+import Footer from '@components/common/Footer.svelte'
+import Header from '@components/common/Header.svelte'
 </script>
 
 <Header />
