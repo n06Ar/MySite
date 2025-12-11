7ebfb76f7e95be3dd5088e9679e7697d2678d1d5
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2025-12-11 22:30:11 +0900

    🔥 Remove Flowbite and related dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,26 @@
-import flowbitePlugin from 'flowbite/plugin'
 import type { Config } from 'tailwindcss'
 
 export default {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
 	],
-	darkMode: 'media',
+	darkMode: 'class',
 	theme: {
 		extend: {
 			colors: {
+				brand: {
+					50: '#eef2ff',
+					100: '#e0e7ff',
+					200: '#c7d2fe',
+					300: '#a5b4fc',
+					400: '#818cf8',
+					500: '#6366f1',
+					600: '#4f46e5',
+					700: '#4338ca',
+					800: '#3730a3',
+					900: '#312e81',
+					950: '#1e1b4b',
+				},
 				primary: {
 					50: '#f2f7fb',
 					100: '#e7f0f8',
@@ -27,5 +38,5 @@ export default {
 		},
 	},
 
-	plugins: [flowbitePlugin],
+	plugins: [],
 } as Config
