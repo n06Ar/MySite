9d76dc570bdf0abcd8c819027b748cdda00f1998
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2025-12-11 22:30:43 +0900

    🔥 Remove Header and Footer components using Flowbite

--- src/components/common/Footer.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-import { Footer, FooterCopyright } from 'flowbite-svelte'
-</script>
-
-<Footer class='pt-5 pb-5 mt-auto text-primary-50 bg-primary-950 text-wrap'>
-	<FooterCopyright by='NaoNao' />
-</Footer>
