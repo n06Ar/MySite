ce93fe10faa8324ff9df9f679d0ec7befc9e7c39
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2025-12-28 04:02:08 +0900

    ✨ Add `sanitize-html` for blog content sanitization

--- src/routes/blog/pages/[contentId]/+page.server.ts
+++ src/routes/blog/pages/[contentId]/+page.server.ts
@@ -1,8 +1,47 @@
 import { error } from '@sveltejs/kit'
+import sanitizeHtml from 'sanitize-html'
 import type { Blog } from '$lib/microCMS'
 import { getBlogPageDetail } from '$lib/microCMS'
 // @ts-ignore
 import type { PageServerLoad } from './$types'
+
+const sanitizeOptions: sanitizeHtml.IOptions = {
+	allowedTags: [
+		...sanitizeHtml.defaults.allowedTags,
+		'figure',
+		'figcaption',
+		'h1',
+		'h2',
+		'h3',
+		'h4',
+		'h5',
+		'h6',
+		'img',
+		'pre',
+		'code',
+		'span',
+		'table',
+		'thead',
+		'tbody',
+		'tr',
+		'th',
+		'td',
+		'hr',
+		'br',
+	],
+	allowedAttributes: {
+		a: ['href', 'name', 'target', 'rel'],
+		img: ['src', 'srcset', 'alt', 'title', 'loading', 'width', 'height'],
+		code: ['class'],
+		pre: ['class'],
+		span: ['class'],
+		'*': ['class', 'id'],
+	},
+	allowedSchemes: ['http', 'https', 'mailto'],
+	allowedSchemesByTag: {
+		img: ['http', 'https'],
+	},
+}
 
 export const load: ({
 	params,
@@ -10,7 +49,11 @@ export const load: ({
 	params: { contentId: string }
 }) => Promise<Blog> = async ({ params }: { params: { contentId: string } }) => {
 	try {
-		return await getBlogPageDetail(params.contentId)
+		const blog = await getBlogPageDetail(params.contentId)
+		return {
+			...blog,
+			content: sanitizeHtml(blog.content, sanitizeOptions),
+		}
 	} catch (e: unknown) {
 		let status = 500
 		if ((e as Error).message === 'Not found') {
